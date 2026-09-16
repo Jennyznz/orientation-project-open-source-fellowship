a8de500ccea165f7e25e618db8a82c41a0c37397
barokdg <dagimbarok@gmail.com>
2026-09-16 18:59:21 +0200

    ci: add frontend ci workflow

---
+++ frontend/eslint.config.js
@@ -0,0 +1,22 @@
+import js from "@eslint/js";
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["src/**/*.{js,mjs,cjs,jsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.browser },
+  },
+  {
+    ...pluginReact.configs.flat.recommended,
+    files: ["src/**/*.{js,mjs,cjs,jsx}"],
+    rules: {
+      ...pluginReact.configs.flat.recommended.rules,
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+    },
+  },
+]);
